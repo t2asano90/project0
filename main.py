10809edd0866c81abaa6164337de6ec70669eee3
“t2asano90” <t.asano90@gmail.com>
2025-04-27 13:38:18 +0900

    株価フォーム対応の変更

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 import yfinance as yf
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/stock/{symbol}")
-def get_stock_price(symbol: str):
-    ticker = yf.Ticker(symbol)
-    data = ticker.history(period="1d")
+# スタティックファイル（CSSとか）を使いたいときのために
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-    if data.empty:
-        return {"error": "Symbol not found or no data available"}
+@app.get("/", response_class=HTMLResponse)
+def read_form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
 
-    latest_price = data["Close"].iloc[-1]
-    return {
-        "symbol": symbol.upper(),
-        "latest_price": latest_price
-    }
+@app.post("/get_price", response_class=HTMLResponse)
+def get_price(request: Request, code: str = Form(...)):
+    # コードを判定して日本株 or 米国株対応
+    if code.isdigit():
+        ticker_symbol = f"{code}.T"
+    else:
+        ticker_symbol = code
+
+    stock = yf.Ticker(ticker_symbol)
+    todays_data = stock.history(period='1d')
+    
+    if todays_data.empty:
+        price = "株価データが取得できませんでした。"
+    else:
+        price = todays_data['Close'][0]
+
+    return templates.TemplateResponse("result.html", {"request": request, "code": code, "price": price})
