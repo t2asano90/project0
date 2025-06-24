aa407612ee3b30a25b64703decdaa8940f6e5b2f
“t2asano90” <t.asano90@gmail.com>
2025-06-24 15:18:01 +0900

    “初期画面のエラーの解消”

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from routers import stock
+
+from routers import stock, history
 from db.database import init_db
 
 app = FastAPI()
 
-# ルーティング追加（router定義ファイル）
-app.include_router(stock.router)
-
-# 静的ファイル・テンプレートの設定
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-from models.database import init_db
-
-init_db()
-
-from routers import stock, history
-
+# ルーティング追加
 app.include_router(stock.router)
 app.include_router(history.router)
 
-# アプリ起動時にDB初期化（これを追加）
+# 静的ファイルとテンプレートの設定
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+# アプリ起動時にDB初期化
 @app.on_event("startup")
-def startup_event():
-    init_db()  
+async def startup_event():
+    await init_db()
+
+# ルートエンドポイント（/）を追加
+from db.database import get_latest_searches
+
+@app.get("/")
+async def read_root(request: Request):
+    history = await get_latest_searches()
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "history": history
+    })
